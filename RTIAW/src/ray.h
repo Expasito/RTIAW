759858647780b6c085bae8da6e4cbd0951626ad3
georg <georg@DESKTOP-UMM6V1R>
2022-10-19 12:07:10 -0700

    Added surface normals, somewhat

--- RTIAW/src/ray.h
+++ RTIAW/src/ray.h
@@ -12,6 +12,10 @@ public:
 	point3 origin() const { return orig; }
 	vec3 direction() const { return dir; }
 
+	point3 at(double t) const {
+		return orig + t * dir;
+	}
+
 public:
 	point3 orig;
 	vec3 dir;
